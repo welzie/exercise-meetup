bb3e1617ef3a9cda697727a0e89588620458825c
Kelly Sorbello <kkellxsorbello@aol.com>
2018-07-19 13:38:51 -0500

    project shell created

---
+++ src/test/java/org/launchcode/exercisemeetup/ExerciseMeetupApplicationTests.java
@@ -0,0 +1,16 @@
+package org.launchcode.exercisemeetup;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ExerciseMeetupApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
