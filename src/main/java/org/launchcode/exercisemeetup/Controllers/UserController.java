5648bbf5c34733afe0c501dc6122a604621f84ef
Aida <aida.moussalli@gmail.com>
2018-07-26 15:07:08 -0500

    sign-in work in progress

--- src/main/java/org/launchcode/exercisemeetup/Controllers/UserController.java
+++ src/main/java/org/launchcode/exercisemeetup/Controllers/UserController.java
@@ -1,8 +1,13 @@
 package org.launchcode.exercisemeetup.Controllers;
 
+import org.launchcode.exercisemeetup.models.forms.LoginForm;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 @RequestMapping("main")
@@ -18,4 +23,25 @@ public class UserController {
         model.addAttribute("title", "New User Registration");
     return "main/register";
     }
+
+    @RequestMapping(value = "login", method = RequestMethod.GET)
+    public String login(Model model) {
+        model.addAttribute("title","Please Login");
+    return "main/login";
+    }
+
+    @RequestMapping(value = "login", method = RequestMethod.POST)
+    public String login(@ModelAttribute LoginForm form, HttpServletRequest request) {
+
+        User theUser = userDao.findByUsername(form.getUsername());
+        String password = form.getPassword();
+
+        setUserInSession(request.getSession(), theUser);
+
+
+        return "redirect:";
+
+    }
+
+
 }
