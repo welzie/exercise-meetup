212b29d7c17406f6d7c3653b2a6dcb53fdaa158e
Aida <aida.moussalli@gmail.com>
2018-07-30 14:33:17 -0500

    Finishing login stuff

--- src/main/java/org/launchcode/exercisemeetup/Controllers/UserController.java
+++ src/main/java/org/launchcode/exercisemeetup/Controllers/UserController.java
@@ -6,6 +6,7 @@ import org.launchcode.exercisemeetup.Models.forms.LoginForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -35,19 +36,36 @@ public class UserController extends org.launchcode.springfilterbasedauth.control
 
     @RequestMapping(value = "login", method = RequestMethod.GET)
     public String login(Model model) {
+        model.addAttribute(new LoginForm());
         model.addAttribute("title","Please Login");
     return "main/login";
     }
 
     @RequestMapping(value = "login", method = RequestMethod.POST)
-    public String login(@ModelAttribute LoginForm form, HttpServletRequest request) {
+    public String login(@ModelAttribute LoginForm form, Errors errors, HttpServletRequest request) {
+
+
+        if (errors.hasErrors()) {
+            return "login";
+        }
 
         User theUser = userDao.findByusername(form.getUsername());
         String password = form.getPassword();
 
+        if(theUser == null) {
+
+            errors.rejectValue("username", "user.invalid", "The given username does not exist");
+            return "login";
+        }
+
+        if (!theUser.isMatchingPassword(password)) {
+            errors.rejectValue("password","password.invalid","Invalid password");
+            return "login";
+        }
+
         setUserInSession(request.getSession(), theUser);
 
-    return "main/index";
+    return "redirect:";
 
     }
 
