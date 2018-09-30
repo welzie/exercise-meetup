882d958b24dcab9a0dfe9f51c62bd79e09bee686
Aida <aida.moussalli@gmail.com>
2018-09-30 14:57:30 -0500

    Working on viewImage controller determining appropriate method

--- src/main/java/org/launchcode/exercisemeetup/Controllers/UploadFileController.java
+++ src/main/java/org/launchcode/exercisemeetup/Controllers/UploadFileController.java
@@ -5,18 +5,24 @@ import org.launchcode.exercisemeetup.Models.User;
 import org.launchcode.exercisemeetup.Models.data.FileRepository;
 import org.launchcode.exercisemeetup.Models.forms.FileModel;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ByteArrayResource;
 import org.springframework.core.io.InputStreamResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.Base64Utils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
-import sun.misc.IOUtils;
 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import java.io.ByteArrayInputStream;
+import java.io.FileInputStream;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 
 @RestController
@@ -79,11 +85,16 @@ public class UploadFileController extends AbstractController {
 
     @GetMapping(value ="/viewfile/image/")
     @ResponseBody
-    public ResponseEntity <InputStreamResource> viewImage (@RequestParam int id) throws Exception {
+    public ResponseEntity<InputStreamResource> viewImage (@RequestParam int id) throws Exception {
 
         FileModel fileModel = fileRepository.findById(id);
 
-     return new InputStreamResource(.rfileModel);
+        return ResponseEntity.ok()
+                .contentLength(fileModel.getLength())
+                .contentType(MediaType.parseMediaType(fileModel.getContentType()))
+                .body(new InputStreamResource(fileModel.getInputStream()));
+
+
 
 
     }
